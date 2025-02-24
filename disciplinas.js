d53d0ff0c644a7eaad58b8d18d4f69079719b9cf
Arthur Dionizio <medardath@icloud.com>
2025-02-24 17:52:23 -0300

    Verificando a existencia do professor

--- disciplinas.js
+++ disciplinas.js
@@ -7,9 +7,19 @@ app.use(express.json());
 
 
 app.post('/disciplinas', async (req, res) => {
+    
     const { name, professorId } = req.body;
 
     if (!name || !professorId) {
         return res.status(400).json({ error: 'Nome da disciplina e ID do professor são obrigatórios' });
+    }
+
+
+     const professor = await prisma.professor.findUnique({
+        where: { id: professorId }
+    });
+
+    if (!professor) {
+        return res.status(404).json({ error: 'Professor não encontrado' });
     }
 });
