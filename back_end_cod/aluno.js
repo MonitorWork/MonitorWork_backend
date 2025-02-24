d089c3007d5aa85e6bcc9df9b5a939f6b0463fde
Matheus <Matheus.rdeoliveira77@gmail.com>
2025-02-24 10:07:14 -0300

    declarando a porta listen para o servidor local

--- back_end_cod/aluno.js
+++ back_end_cod/aluno.js
@@ -4,3 +4,8 @@ import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
 const app = express();
 app.use(express.json())
+
+
+app.listen(3000, () => {
+    console.log('Servidor rodando na porta 3000');
+});
