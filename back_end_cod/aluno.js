6213e9d58a5b240c0c556dc6470c683572b8a015
Matheus <Matheus.rdeoliveira77@gmail.com>
2025-02-24 10:09:31 -0300

    criar os cadastro no banco de dados

--- back_end_cod/aluno.js
+++ back_end_cod/aluno.js
@@ -1,10 +1,11 @@
 import express from 'express';
 import { PrismaClient } from '@prisma/client';
-
 const prisma = new PrismaClient();
 const app = express();
-app.use(express.json())
+app.use(express.json());
 
+
+pp.post('/alunos', async (req, res)) //criar o campo que comunica com o banco
 
 app.listen(3000, () => {
     console.log('Servidor rodando na porta 3000');
