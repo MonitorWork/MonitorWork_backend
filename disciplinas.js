075c2bb1e8b3557c03ad64b7d156add878d0c79c
Arthur Dionizio <medardath@icloud.com>
2025-02-24 17:55:38 -0300

    Vinculando matéria e professor

--- disciplinas.js
+++ disciplinas.js
@@ -5,21 +5,30 @@ const prisma = new PrismaClient();
 const app = express();
 app.use(express.json());
 
-
+/** Criar uma matéria vinculada a um professor */
 app.post('/disciplinas', async (req, res) => {
-    
     const { name, professorId } = req.body;
 
     if (!name || !professorId) {
         return res.status(400).json({ error: 'Nome da disciplina e ID do professor são obrigatórios' });
     }
 
-
-     const professor = await prisma.professor.findUnique({
+    // Verificar se o professor existe
+    const professor = await prisma.professor.findUnique({
         where: { id: professorId }
     });
 
     if (!professor) {
         return res.status(404).json({ error: 'Professor não encontrado' });
     }
+
+    const disciplina = await prisma.discipline.create({
+        data: {
+            name,
+            professorId
+        },
+        include: { professor: true } // Retorna os dados do professor junto com a disciplina
+    });
+
+    res.status(201).json(disciplina);
 });
