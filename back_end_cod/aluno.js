b352e0ecbf5a6d0a2c12929a43adf5bf9a02d961
Matheus <Matheus.rdeoliveira77@gmail.com>
2025-02-24 12:06:23 -0300

    colocar um sistema de segurança capturar senha

--- back_end_cod/aluno.js
+++ back_end_cod/aluno.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import { PrismaClient } from '@prisma/client';
+import bcrypt from 'bcrypt';
 const prisma = new PrismaClient();
 const app = express();
 app.use(express.json());
