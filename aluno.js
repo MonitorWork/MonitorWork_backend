4aa036193d41b75b8facf3622c418aa499decd84
MatheusR <matheus.rdeoliveira77@gmail.com>
2025-02-25 12:49:55 -0300

    att

--- aluno.js
+++ aluno.js
@@ -7,12 +7,6 @@ dotenv.config();
 
 import cors from 'cors';
 
-app.use(cors({
-    origin: '*', // Permite requisições de qualquer origem (modifique conforme necessário)
-    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Métodos HTTP permitidos
-    allowedHeaders: ['Content-Type', 'Authorization'] // Cabeçalhos permitidos
-}));
-
 const app = express();
 const prisma = new PrismaClient();
 app.use(cors({
